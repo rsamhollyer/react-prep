bc7ad806bcf542d5b5af23dcaaad5d240e0588f2
Sam Hollyer <rsamhollyer@gmail.com>
2021-01-19 12:07:04 -0600

    work on promises/fetch added

--- scripts/app.js
+++ scripts/app.js
@@ -1,1 +1,26 @@
-console.log(`Hello World`);
+// console.log(`Hello World`);
+/* Show Joke on Page
+1. Make request with fetch()
+2. grap .joke 
+3. render it to the private
+*/
+
+const url = `https://icanhazdadjoke.com/`;
+
+const jokes = fetch(url, {
+	method: "GET",
+	headers: {
+		Accept: "application/json",
+	},
+})
+	.then((response) => response.json())
+	.then((data) => {
+		// console.log(data);
+		return data;
+	})
+	.then((theJoke) => {
+		console.log(theJoke);
+	})
+	.catch((err) => console.log(err));
+
+// console.log(jokes);
