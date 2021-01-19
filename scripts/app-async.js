be9ff62cfe3a23d5b03bfd5538c149fb6a6e3110
Sam Hollyer <rsamhollyer@gmail.com>
2021-01-19 16:24:38 -0600

    add more stuff

--- scripts/app-async.js
+++ scripts/app-async.js
@@ -1,6 +1,28 @@
+/*
+
+How do you get an 'as-you-type' feature in your front end app?
+Example: as you type a suername into a sgnup form ite tells you if that name is taken.
+
+1. add event listener
+2. this will use a fetch() in the callback to check an end-point to check if that value exists
+3. render the response
+
+*/
+
+function main() {
+	const jokeButton = document.querySelector("#new-joke");
+	jokeButton.addEventListener("click", async (event) => {
+		event.preventDefault();
+		const joke = await getJoke();
+		renderJoke(joke);
+	});
+}
+
+main();
+
 const url = `https://icanhazdadjoke.com/`;
 
-async function main() {
+async function getJoke() {
 	const response = await fetch(url, {
 		method: "GET",
 		headers: {
@@ -9,8 +31,16 @@ async function main() {
 	});
 
 	const jokeData = await response.json();
-	console.log(jokeData);
-	console.log(jokeData.joke);
+	return jokeData.joke;
 }
 
-main();
+async function renderJoke(joke) {
+	// async beacuse we will await getjoke
+	//create the dom element
+	const p = document.createElement("p");
+	//put joke text into it
+	p.textContent = joke;
+	//attach it to an existing dom element
+	const root = document.querySelector("#root");
+	root.appendChild(p);
+}
